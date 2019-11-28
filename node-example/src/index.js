92b3cc23dd1ed5f849ebe6c6f48b38b91ee63ca4
Matsuuu <huhta.matias@gmail.com>
2019-11-29 00:00:44 +0200

    WIP: Feat: Work started on a initializable router. - Made router constructable, and then later on initializable allowing setting attributes before init - Created new options 	- Allow waiting until loading is done 	- Allow debugging messages

--- node-example/src/index.js
+++ node-example/src/index.js
@@ -1,13 +1,14 @@
 import { LitElement, html } from 'lit-element';
-import SimplrRouter from "../../lib/simplr-router";
-import "./views/simplr-router-link";
-import routes from "../simplr-routes";
+import SimplrRouter from '../../lib/simplr-router';
+import './views/simplr-router-link';
+import routes from '../simplr-routes';
+import SimplrFoo from './views/simplr-foo';
+import { TransitionSpeed } from '../../lib/simplr-router-transition-speed';
+import { TransitionDirection } from '../../lib/simplr-router-transition-direction';
 
 class SimplrRouterIndex extends LitElement {
     static get properties() {
-        return {
-
-        };
+        return {};
     }
 
     constructor() {
@@ -15,13 +16,27 @@ class SimplrRouterIndex extends LitElement {
     }
 
     firstUpdated(_changedProperties) {
-        SimplrRouter.init(this, routes);
+        let router = new SimplrRouter({
+            activeView: this,
+            routes: routes,
+            notFoundAction: () => {
+                alert('Page not found');
+            },
+            forbiddenAction: () => {
+                alert('Forbidden');
+            },
+            transitionSpeed: TransitionSpeed.FAST,
+            transitionDirection: TransitionDirection.RIGHT,
+            debugging: true,
+            waitForLoad: true,
+        });
+
+        router.init();
     }
 
     createRenderRoot() {
         return this;
     }
-
 
     render() {
         return html`
@@ -32,10 +47,9 @@ class SimplrRouterIndex extends LitElement {
             <a href="foo/12/info" data-simplr-route>foo 12 info</a>
             <a href="baz" data-simplr-route>baz</a>
             <a href="https://www.google.com/webhp?ie=UTF-8&rct=j">Google</a>
-            <p @click="${() => SimplrRouter.navigateToPath("foo/12")}">asd</p>
+            <p @click="${() => SimplrRouter.navigateToPath('foo/12')}">asd</p>
         `;
     }
-
 }
 
-customElements.define("simplr-router-index", SimplrRouterIndex);
+customElements.define('simplr-router-index', SimplrRouterIndex);
--- node-example/src/views/simplr-foo.js
+++ node-example/src/views/simplr-foo.js
@@ -1,20 +1,57 @@
 import {html, LitElement} from 'lit-element';
 import "./simplr-router-link";
 
-class SimplrFoo extends LitElement {
+export default class SimplrFoo extends LitElement {
     static get properties() {
         return {
-            lang: {type: String}
+            lang: {type: String},
+            loadImages: {type: Boolean},
+            loadMoreImages: {type: Boolean},
+            loadEvenMoreImages: {type: Boolean},
+            isLoading: {type: Boolean}
         };
     }
 
     constructor() {
         super();
+        console.log("I have been constructed");
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+        console.log("I have connected");
+        this.innerHTML = "<p>test</p>";
+    }
+
+    firstUpdated(_changedProperties) {
+        this.doWaiting();
+    }
+
+    async doWaiting() {
+        let loadTime = 500;
+        this.isLoading = true;
+        await this.wait(loadTime);
+        this.loadImages = true;
+        await this.wait(loadTime);
+        this.loadMoreImages = true;
+        await this.wait(loadTime);
+        this.loadEvenMoreImages = true;
+        this.isLoading = false;
+    }
+
+    wait(ms) {
+        return new Promise((resolve) => {setTimeout(resolve, ms)});
     }
 
     render() {
         return html`
             <p>Foo</p>
+            ${this.loadImages ? html`
+            <img style="width: 200px" src="https://i.imgur.com/2ZDbOok.jpg"/>` : ''}
+            ${this.loadMoreImages ? html`
+            <img style="width: 200px" src="https://i.imgur.com/InFyAVW.jpg"/>` : ''}
+            ${this.loadEvenMoreImages ? html`
+            <img style="width: 200px" src="https://upload.wikimedia.org/wikipedia/commons/c/cc/ESC_large_ISS022_ISS022-E-11387-edit_01.JPG"/>`: ''}
             <simplr-router-link route="foo/12" title="Foo 12"></simplr-router-link>
         `;
     }
