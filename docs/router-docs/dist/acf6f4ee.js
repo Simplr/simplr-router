ff674314ae2ee23073e54dfd1bb9651cb9ed6f34
Matsuuu <huhta.matias@gmail.com>
2020-09-15 21:07:44 +0300

    Update docs

---
+++ docs/router-docs/dist/acf6f4ee.js
@@ -0,0 +1,48 @@
+import{L as t,c as e}from"./0b10f69c.js";let i,d=t=>t;class r extends t{static get styles(){return e(i||(i=d`
+            :host {
+                min-height: 80vh;
+                display: flex;
+                flex-direction: column;
+                align-items: flex-start;
+                justify-content: flex-start;
+                font-size: 1.4rem;
+                color: #1a2b42;
+                margin: 2.5% auto;
+                width: 60%;
+            }
+
+            highlight-js {
+                width: 80%;
+                text-align: start;
+                font-size: 1.1rem;
+            }
+
+            table {
+                background: #fff;
+                font-size: 1.2rem;
+                border: 2px solid #484848;
+                border-radius: 4px;
+                padding: 1rem;
+            }
+
+            th,
+            td {
+                border-bottom: 1px solid #484848;
+                padding: 1rem;
+            }
+
+            td:nth-child(1) {
+                width: 20%;
+                border-right: 1px solid #484848;
+            }
+            td:nth-child(2) {
+                width: 35%;
+            }
+            td:nth-child(3) {
+                border-left: 1px solid #484848;
+            }
+
+            tr:last-child td {
+                border-bottom: unset;
+            }
+        `))}}export{r as V};
