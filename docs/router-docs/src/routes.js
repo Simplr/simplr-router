195fbac3c9c677f76cc64a6c9650dd9c870660ad
Matsuuu <huhta.matias@gmail.com>
2020-09-15 21:36:24 +0300

    We'll host it elsewhere

--- docs/router-docs/src/routes.js
+++ docs/router-docs/src/routes.js
@@ -1,48 +1,48 @@
 const routes = [
     {
-        path: 'simplr-router',
+        path: '',
         component: 'router-docs-root',
         import: () => import('./views/root-view.js'),
         name: 'Home',
     },
     {
-        path: 'simplr-router/getting-started',
+        path: '/getting-started',
         component: 'router-docs-getting-started',
         import: () => import('./views/getting-started.js'),
         name: 'Getting started',
     },
     {
-        path: 'simplr-router/api-description',
+        path: '/api-description',
         component: 'router-docs-api-description',
         import: () => import('./views/api-description.js'),
         name: 'API description',
     },
     {
-        path: 'simplr-router/recipes',
+        path: '/recipes',
         component: 'router-docs-recipes',
         import: () => import('./views/docs-recipes.js'),
         name: 'Recipes',
         routes: [
             {
-                path: 'simplr-router/sub-routes',
+                path: '/sub-routes',
                 component: 'router-docs-sub-routes',
                 import: () => import('./views/docs-sub-routes.js'),
                 name: 'Sub routes',
             },
             {
-                path: 'simplr-router/guards',
+                path: '/guards',
                 component: 'router-docs-guards',
                 import: () => import('./views/docs-guard.js'),
                 name: 'Guards',
             },
             {
-                path: 'simplr-router/error-pages',
+                path: '/error-pages',
                 component: 'router-docs-error-pages',
                 import: () => import('./views/docs-error-pages.js'),
                 name: 'Error pages',
             },
             {
-                path: 'simplr-router/code-splitting',
+                path: '/code-splitting',
                 component: 'router-docs-code-splitting',
                 import: () => import('./views/docs-code-splitting.js'),
                 name: 'Code splitting',
@@ -50,7 +50,7 @@ const routes = [
         ],
     },
     {
-        path: 'simplr-router/quick-start',
+        path: '/quick-start',
         component: 'router-docs-quick-start',
         import: () => import('./views/quick-start.js'),
         name: 'Quick start',
