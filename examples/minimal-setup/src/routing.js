9e940a62fe76e046255442810418c117ea50aba9
Matsuuu <huhta.matias@gmail.com>
2021-10-21 10:54:55 +0300

    Redirect docs

--- examples/minimal-setup/src/routing.js
+++ examples/minimal-setup/src/routing.js
@@ -22,6 +22,12 @@ const routes = [
         },
         component: "dynamic-view"
     },
+    {
+        path: "/do-redirect", redirect: "/example"
+    },
+    {
+        path: "/name-redirect", redirect: { name: "Example" }
+    }
 ];
 
 new SimplrRouter({ routes }).init();
