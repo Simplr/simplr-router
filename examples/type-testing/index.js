61f8d1b8f536f3beb9dd1f2fdc6746392604ac36
Matsuuu <huhta.matias@gmail.com>
2021-12-15 23:08:11 +0200

    Enhance type support

---
+++ examples/type-testing/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var router_1 = require("@simplr-wc/router");
+var options = {};
+var router = new router_1.SimplrRouter(options);
---
+++ examples/type-testing/index.ts
@@ -0,0 +1,8 @@
+import { SimplrRouter, SimplrRouterOptions } from "@simplr-wc/router";
+
+const options = {
+
+} as SimplrRouterOptions;
+
+const router = new SimplrRouter(options);
+
