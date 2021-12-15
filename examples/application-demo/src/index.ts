30cd5627935ceebdee3319358c070ef718405843
Matsuuu <huhta.matias@gmail.com>
2021-12-15 23:24:26 +0200

    Fix typing all around the project

---
+++ examples/application-demo/src/index.ts
@@ -0,0 +1,7 @@
+import { SimplrRouter, SimplrRouterOptions } from "@simplr-wc/router";
+
+const routerOptions: SimplrRouterOptions = {
+    routes: []
+};
+
+const router = new SimplrRouter(routerOptions);
